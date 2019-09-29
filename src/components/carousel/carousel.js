7cfdc672d74ee2e9e823de045503a99ee4ee6bd6
Md. Fahim Shahrier Rasel <fahimshahrierrasel@users.noreply.github.com>
2019-09-30 01:47:12 +0600

    Refactor file rename fail successful rename and carousel style updated (#11)

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -6,7 +6,15 @@ function Carousel({ image, title }) {
   return (
     <div className="carousel">
       <div className="overlay">
-        <h2>{title}</h2>
+        <div className="content">
+          <h2>{title}</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti a
+            distinctio dolorum laborum et beatae accusantium cum quasi,
+            reprehenderit quaerat incidunt minima animi veniam laboriosam
+            laudantium eveniet. Veniam, voluptas. Ullam!
+          </p>
+        </div>
         <div className="btn btn--animated">Play Trailer</div>
       </div>
       <img src={image} alt={title} />
