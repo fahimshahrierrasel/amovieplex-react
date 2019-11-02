9e5b08162a7f1f8d1a579f91ec6bb4cc7e98141f
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2019-11-03 00:33:24 +0600

    show menu on hovering user-info add

--- src/views/components/admin/header/header.js
+++ src/views/components/admin/header/header.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./header.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faBell, faCog } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faBell,
+  faCog,
+  faIdCard,
+  faSignOutAlt
+} from "@fortawesome/free-solid-svg-icons";
 import { UserAvatar } from "../../../../constants/images";
 
 const AdminHeader = () => {
@@ -13,6 +19,21 @@ const AdminHeader = () => {
       <div className="user-info">
         <img className="avatar" src={UserAvatar} alt="User Avatar" />
         <span>Admin</span>
+      </div>
+      <div className="user-action__container">
+        <div className="user-actions">
+          <button>
+            <FontAwesomeIcon icon={faIdCard} className="header-icon__button" />
+            Profile
+          </button>
+          <button>
+            <FontAwesomeIcon
+              icon={faSignOutAlt}
+              className="header-icon__button"
+            />
+            Logout
+          </button>
+        </div>
       </div>
     </div>
   );
