f7b6fe81dbd3cee1e7cd419b30ad28fd4a79b9d8
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2019-11-02 20:41:40 +0600

    admin header, sidebar and footer position fix and ui refreshment

--- src/views/components/admin/sidebar/sidebar.js
+++ src/views/components/admin/sidebar/sidebar.js
@@ -8,24 +8,39 @@ import "./sidebar.scss";
 
 const Sidebar = () => {
   const location = useLocation();
+  const items = [];
+  for (let i = 0; i < 100; i++) {
+    items.push(i);
+  }
   return (
     <div className="sidebar__container">
       <div className="sidebar-header">
         <img src={MoviePlexLogo} alt="MoviePlex Logo" className="logo" />
         <span>A Movie Plex</span>
       </div>
-      <SidebarItem
-        title="Dashboard"
-        icon={faTachometerAlt}
-        route={Routes.ADMIN_DASHBOARD()}
-        active={location.pathname === Routes.ADMIN_DASHBOARD()}
-      />
-      <SidebarItem
-        title="Movies"
-        icon={faFilm}
-        route={Routes.ADMIN_MOVIES()}
-        active={location.pathname === Routes.ADMIN_MOVIES()}
-      />
+      <div className="sidebar-item__list">
+        <SidebarItem
+          title="Dashboard"
+          icon={faTachometerAlt}
+          route={Routes.ADMIN_DASHBOARD()}
+          active={location.pathname === Routes.ADMIN_DASHBOARD()}
+        />
+        <SidebarItem
+          title="Movies"
+          icon={faFilm}
+          route={Routes.ADMIN_MOVIES()}
+          active={location.pathname === Routes.ADMIN_MOVIES()}
+        />
+        {items.map(item => (
+          <SidebarItem
+            key={item}
+            title={`Item ${item}`}
+            icon={faFilm}
+            route={Routes.ADMIN_MOVIES() + `/${item}`}
+            active={location.pathname === Routes.ADMIN_MOVIES() + `/${item}`}
+          />
+        ))}
+      </div>
     </div>
   );
 };
